74ff466b6a21c7c8d86fa0fef1b929931d8508d9
Yash Ajitsaria <ajitsariayash320@gmail.com>
2023-05-18 12:39:50 +0530

    added function to get a single post by id

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
+# from random import randrange
 
 class Post(BaseModel):
     title: str
@@ -10,15 +11,30 @@ class Post(BaseModel):
 
 app = FastAPI()
 
+myPosts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "title of post 2", "content": "content of post 2", "id": 2}]
+
+def find_post(id):
+    for post in myPosts:
+        if post['id'] == id:
+            return post
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "These are your posts"}
+    return {"data": myPosts}
 
 @app.post("/posts")
 def create_posts(post: Post):
-    print(post.dict())
-    return {"data": f"title: {post.title}, content: {post.content}"}
+    post_dict = post.dict()
+    post_dict["id"] = len(myPosts) + 1 #randrange(0, 100000)
+    myPosts.append(post_dict)
+    return {"data": post_dict}
+
+@app.get("/posts/{id}") #path params are always str
+def get_post(id: int):
+    post = find_post(id)
+    print(post)
+    return {"post_detail": post}
