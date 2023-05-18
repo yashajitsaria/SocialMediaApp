be7c8634c2b8d83cbe9c5ebb147660f800bad7b5
Yash Ajitsaria <ajitsariayash320@gmail.com>
2023-05-18 12:12:17 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from typing import Optional
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
 
 class Post(BaseModel):
     title: str
     content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 app = FastAPI()
 
@@ -11,6 +14,11 @@ app = FastAPI()
 def root():
     return {"message": "Hello World"}
 
+@app.get("/posts")
+def get_posts():
+    return {"data": "These are your posts"}
+
 @app.post("/posts")
 def create_posts(post: Post):
+    print(post.dict())
     return {"data": f"title: {post.title}, content: {post.content}"}
