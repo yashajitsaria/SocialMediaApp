be7ae6909ec37b32e1e5e5cce9becd390003c620
Yash Ajitsaria <ajitsariayash320@gmail.com>
2023-05-19 16:59:11 +0530

    cleaning

--- app/main.py
+++ app/main.py
@@ -5,19 +5,11 @@ from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
 from . import models
-from .database import engine, SessionLocal
+from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 #Post schema
 class Post(BaseModel):
