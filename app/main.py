ade18938485173b439056d6632b36d06e5570cef
Yash Ajitsaria <ajitsariayash320@gmail.com>
2023-05-19 23:17:34 +0530

    updated get_post using get_db dependency

--- app/main.py
+++ app/main.py
@@ -62,9 +62,10 @@ def create_posts(post: Post, db: Session = Depends(get_db)):
 
 #Get a Post
 @app.get("/posts/{id}")
-def get_post(id: int):
-    cursor.execute("SELECT * FROM posts WHERE id = %s", (str(id)))
-    post = cursor.fetchone()
+def get_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("SELECT * FROM posts WHERE id = %s", (str(id)))
+    # post = cursor.fetchone()
+    post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
     return {"post_detail": post}
