66343fa7f365801b5db636242af1cff3388c20ce
Yash Ajitsaria <ajitsariayash320@gmail.com>
2023-05-24 17:08:26 +0530

    added auth.router

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from psycopg2.extras import RealDictCursor
 import time
 from . import models
 from .database import engine
-from .routes import post, user
+from .routes import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -24,6 +24,7 @@ while True:
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 #Hello World
 @app.get("/")
