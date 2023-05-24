42c2409102d4d2400e8dd7e23a96fe19b510970f
Yash Ajitsaria <ajitsariayash320@gmail.com>
2023-05-24 15:52:32 +0530

    added get_user path operation

--- app/main.py
+++ app/main.py
@@ -97,3 +97,13 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(newUser)
     return newUser
+
+#Get a User
+@app.get("/users/{id}")
+def get_user(id:int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user with id: {id} does not exist")
+    
+    return user
