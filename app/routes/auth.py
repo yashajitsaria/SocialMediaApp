2c2b01a3837940c1e8762b8d78ed6ba69ca88110
Yash Ajitsaria <ajitsariayash320@gmail.com>
2023-05-24 17:36:07 +0530

    updated login route and used password req form dependency

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,14 +1,15 @@
-from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from .. import database, schemas, models, utils
+from .. import database, schemas, models, utils, oauth2
 
 router = APIRouter(
     tags=['Authentication']
 )
 
 @router.post("/login")
-def login(userCredentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == userCredentials.email).first()
+def login(userCredentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email == userCredentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
@@ -16,5 +17,5 @@ def login(userCredentials: schemas.UserLogin, db: Session = Depends(database.get
     if not utils.verify(userCredentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
 
-    #create jwt token and return it
-    return {"token": "example token"}
+    accessToken = oauth2.create_access_token(payload={"user_id": user.id})
+    return {"accessToken": accessToken, "token_type": "bearer"}
