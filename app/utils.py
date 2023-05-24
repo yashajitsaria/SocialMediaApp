1981364533084cae60b6ff1a9b62a13e26c268ed
Yash Ajitsaria <ajitsariayash320@gmail.com>
2023-05-24 17:09:34 +0530

    implemented the function to verify hashed password

--- app/utils.py
+++ app/utils.py
@@ -4,3 +4,6 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash(password: str):
     return pwd_context.hash(password)
+
+def verify(plainPassword: str, hashedPassword: str):
+    return pwd_context.verify(plainPassword, hashedPassword)
