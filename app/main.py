45bd0518ecbe27aace238451c3b6ec73bb3ceabc
Yash Ajitsaria <ajitsariayash320@gmail.com>
2023-05-24 16:07:51 +0530

    added routes and refactored

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends
+from fastapi import FastAPI
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
-from sqlalchemy.orm import Session
-from . import models, schemas, utils
-from .database import engine, get_db
-from typing import List
+from . import models
+from .database import engine
+from .routes import post, user
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -23,87 +22,10 @@ while True:
         print("Error: ", err)
         time.sleep(2)
 
+app.include_router(post.router)
+app.include_router(user.router)
+
 #Hello World
 @app.get("/")
 def root():
     return {"message": "Hello World"}
-
-#Get all Posts
-@app.get("/posts", response_model=List[schemas.Post])
-def get_all_posts(db: Session = Depends(get_db)):
-    # cursor.execute("SELECT * FROM posts")
-    # posts = cursor.fetchall()
-    posts = db.query(models.Post).all()
-    return posts
-
-#Create a Post
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute("INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *", (post.title, post.content, post.published))
-    # post = cursor.fetchone()
-    # conn.commit()
-    newPost = models.Post(**post.dict())
-    db.add(newPost)
-    db.commit()
-    db.refresh(newPost)
-    return newPost
-
-#Get a Post
-@app.get("/posts/{id}", response_model=schemas.Post)
-def get_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("SELECT * FROM posts WHERE id = %s", (str(id)))
-    # post = cursor.fetchone()
-    post = db.query(models.Post).filter(models.Post.id == id).first()
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-    return post
-
-#Delete a Post
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("DELETE FROM posts WHERE id = %s RETURNING *", (str(id),))
-    # post = cursor.fetchone()
-    post = db.query(models.Post).filter(models.Post.id == id)
-    if post.first() == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
-    post.delete(synchronize_session=False)
-    db.commit()
-    # conn.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-#Update a Post
-@app.put("/posts/{id}", response_model=schemas.Post)
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute("UPDATE posts SET title=%s, content=%s, published=%s WHERE id = %s RETURNING *", (post.title, post.content, post.published, str(id)))
-    # post = cursor.fetchone()
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    post = post_query.first()
-    if post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
-    post_query.update(updated_post.dict(), synchronize_session=False)
-    db.commit()
-    # conn.commit()
-    return post_query.first()
-
-#Create a user
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    #hash the password
-    hashedPassword = utils.hash(user.password)
-    user.password = hashedPassword
-
-    newUser = models.User(**user.dict())
-    db.add(newUser)
-    db.commit()
-    db.refresh(newUser)
-    return newUser
-
-#Get a User
-@app.get("/users/{id}", response_model=schemas.UserResponse)
-def get_user(id:int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user with id: {id} does not exist")
-    
-    return user
