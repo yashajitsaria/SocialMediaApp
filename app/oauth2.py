ee461e86075731ee630806c14f7f60089536ef38
Yash Ajitsaria <ajitsariayash320@gmail.com>
2023-05-25 13:03:13 +0530

    updated get_current_user function

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,8 +1,9 @@
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 from jose import JWSError, jwt
 from datetime import datetime, timedelta
-from . import schemas
+from . import schemas, database, models
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
@@ -34,7 +35,10 @@ def verify_access_token(token: str, credentials_exception):
 
     return token_data
 
-def get_current_user(token: str = Depends(o2auth_scheme)):
+def get_current_user(token: str = Depends(o2auth_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW_Authenticate": "Bearer"})
 
-    return verify_access_token(token, credentials_exception)
+    token_data = verify_access_token(token, credentials_exception)
+    user = db.query(models.User).filter(models.User.id == token_data.id).first()
+    
+    return user
