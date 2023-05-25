337d757426387e0136dc8130516e18703062cf4f
Yash Ajitsaria <ajitsariayash320@gmail.com>
2023-05-25 12:15:35 +0530

    created schemas for token and tokenData

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 from pydantic import BaseModel, EmailStr
 
 #Post Schema
@@ -33,3 +34,10 @@ class UserResponse(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Optional[str]
