f8810a64feb296f7f359c40fa11fadffd87a2c9f
Yash Ajitsaria <ajitsariayash320@gmail.com>
2023-05-25 12:14:55 +0530

    updated status codes for invalid creds

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -12,10 +12,10 @@ def login(userCredentials: OAuth2PasswordRequestForm = Depends(), db: Session = 
     user = db.query(models.User).filter(models.User.email == userCredentials.username).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
     
     if not utils.verify(userCredentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
 
     accessToken = oauth2.create_access_token(payload={"user_id": user.id})
     return {"accessToken": accessToken, "token_type": "bearer"}
